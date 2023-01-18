60559636cf9a6db98ff3bf3ca626b6b6e79a8284
Gavin Mendel-Gleason <gavin@terminusdb.com>
2023-01-18 14:42:11 +0100

    Upgrade docker

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,8 @@ WORKDIR /app/terminusdb-10-to-11
 RUN cargo build --release
 
 FROM debian:bullseye
-WORKDIR /app/terminusdb-10-to-11
-COPY --from=base /app/terminusdb-10-to-11/target/release/terminusdb-10-to-11 terminusdb-10-to-11
+WORKDIR /app/terminusdb-upgrade
+RUN mkdir bin
+COPY --from=base /app/terminusdb-10-to-11/target/release/terminusdb-10-to-11 bin/terminusdb-10-to-11
+COPY upgrade.sh upgrade.sh
+CMD ["/app/terminusdb-upgrade/upgrade.sh"]
---
+++ upgrade.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+SOURCE="$TERMINUSDB_QUICKSTART_STORAGE/db"
+SOURCE_BACKUP="$SOURCE.10.bkp"
+TARGET="$TERMINUSDB_QUICKSTART_STORAGE/db-11"
+WORK="$TERMINUSDB_QUICKSTART_STORAGE/work-10-to-11"
+
+if [ ! -e "$SOURCE/STORAGE_VERSION" ]
+then
+    echo "There is no store discovered at $SOURCE"
+    exit 1
+fi
+
+VERSION=`cat "$SOURCE/STORAGE_VERSION"`
+if [ $VERSION -eq "2" ]
+then
+    echo "Store is already converted!"
+    exit 1
+elif [ $VERSION -eq "1" ]
+then
+    echo "Beginning conversion of TerminusDB 10 store"
+else
+    echo "Store provided is an unrecognized version: $VERSION"
+    exit 1
+fi
+
+if [ -e "$SOURCE_BACKUP" ]
+then
+    echo "There is a source backup already `$SOURCE_BACKUP`, please remove it!"
+    exit 1
+fi
+
+/app/terminusdb-upgrade/bin/terminusdb-10-to-11 convert-store "$SOURCE" "$TARGET" -w "$WORK"
+# Test something
+if [ $? -eq 0 ]
+then
+    echo "Successful conversion of store"
+    mv "$SOURCE" "$SOURCE_BACKUP"
+    mv "$TARGET" "$SOURCE"
+    rm -rf "$WORK"
+else
+    echo "Failed to convert store!";
+fi
